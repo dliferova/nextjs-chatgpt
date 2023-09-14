56b8ac41d480b1f732de5671358496ba098e543c
Daria Liferova <liferova.daria@gmail.com>
2023-09-14 15:15:32 +0200

    Stream the OpenAI response on the front end

--- pages/chat/[[...chatId]].tsx
+++ pages/chat/[[...chatId]].tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import { ChatSidebar } from "../../components/chatSidebar";
+import { streamReader } from "openai-edge-stream";
 
 const ChatPage = () => {
+  const [incomingMessage, setIncomingMessage] = useState("")
   const [messageText, setMessageText] = useState("");
 
-  const handleSubmit = (e: { preventDefault: () => void }) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Message text: ", messageText);
+
+    const response = await fetch(`/api/chat/sendMessage`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(({message: messageText}))
+    });
+
+    const data = response.body;
+
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+
+    await streamReader(reader, (message) => {
+      setIncomingMessage(s => `${s}${message.content}`)
+    })
   };
 
   return (
@@ -18,7 +39,7 @@ const ChatPage = () => {
       <div className="grid h-screen grid-cols-[260px_1fr] ">
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex-1">chat window</div>
+          <div className="flex-1 text-white">{incomingMessage}</div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
